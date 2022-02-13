401da8adc45a14ef6fde06df5712bd00ab58c93e
PedroFeliciano <pedrofeliciano647@gmail.com>
2022-02-12 22:44:43 -0300

    estudos basicos js

--- Desafio 1.1/script.js
+++ Desafio 1.1/script.js
@@ -50,3 +50,38 @@ function Subtracao(a, b)
 {
 return a -b;
 }
+
+
+let preço = 19.90;
+let desconto = 0.4 ;
+
+console.log( preço * (1- desconto) )
+
+ // ulltilizado para saber o tipo//
+console.log(typeof preço)   
+
+// caulculo da circunferencia //
+
+const PI = 3.14;
+
+let raio = 10;
+
+circunferencia = PI *raio  * raio + " m2"
+
+console.log(circunferencia)
+
+// trocar os valores das variaveis //
+
+let a = 7 
+let b = 94
+
+
+let c = a
+
+a=b
+b=c
+
+console.log(a)
+console.log(b)
+
+
